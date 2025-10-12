2c2cee0a14f693df7a7fde42c8990ae5764e8f24
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:51:05 -0300

    Update Casa-at9.py

--- Casa-at9.py
+++ Casa-at9.py
@@ -1,1 +1,7 @@
+P1 = float(input("Digite a nota da primeira avaliação (P1): "))
+P2 = float(input("Digite a nota da segunda avaliação (P2): "))
+Ativ = float(input("Digite a nota das atividades (Ativ): "))
 
+media = (P1 * 4 + P2 * 4 + Ativ * 2) / 10
+
+print(f"A média semestral é: {media:.2f}")
