e2672577626206bad6653c65c2970bd6729cc7fa
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:25:39 -0300

    Update Casa-at1.py

--- Casa-at1.py
+++ Casa-at1.py
@@ -1,3 +1,3 @@
-Reais = float(input("Digite o valor em reais: "))
-Dolar = Reais / 2.40
-print(f"Valor em dolar: {Dolar}")
+reais = float(input("Digite o valor em reais: "))
+dolar = reais / 2.40
+print(f"Valor em dolar: {dolar}")
