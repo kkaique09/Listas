534643b029fd382c8e8ed2af00a45f937410bad4
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:43:59 -0300

    Update Casa-at3.py

--- Casa-at3.py
+++ Casa-at3.py
@@ -1,1 +1,10 @@
-
+altura_parede = float(input("Insira o valor da ALTURA da parede:\n"))
+largura_parede = float(input("Insira o valor da LARGURA da parede:\n"))
+area_parede = altura_parede * largura_parede
+print(f"A Area da parede é: {area_parede:.2f} metros quadrados\n")
+altura_azulejo = float(input("Insira o valor da ALTURA do azulejo:\n"))
+largura_azulejo = float(input("Insira o valor da LARGURA do azulejo:\n"))
+area_azulejo = altura_azulejo * largura_azulejo
+print(f"A Area do azulejo é: {area_azulejo:.2f} metros quadrados\n")
+quantidade_azulejos = area_parede / area_azulejo
+print(f"A Quantidade de azulejos necessario para azulejar a parede é: {quantidade_azulejos}")
