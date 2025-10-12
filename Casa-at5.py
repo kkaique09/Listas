309465bfda3df7eef5dcbbac921ba155a602b1c4
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:49:27 -0300

    Update Casa-at5.py

--- Casa-at5.py
+++ Casa-at5.py
@@ -1,1 +1,6 @@
+massa = float(input("Digite a massa da pessoa (em kg): "))
+altura = float(input("Digite a altura da pessoa (em metros): "))
 
+imc = massa / (altura ** 2)
+
+print(f"O IMC da pessoa é: {imc:.2f}")
