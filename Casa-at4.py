20e056db36aeeea66e3e917084357e3558520668
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:49:06 -0300

    Update Casa-at4.py

--- Casa-at4.py
+++ Casa-at4.py
@@ -1,1 +1,8 @@
+a = float(input("Digite o valor do lado 'a' do retângulo: "))
+b = float(input("Digite o valor do lado 'b' do retângulo: "))
 
+area = a * b
+perimetro = 2 * a + 2 * b
+
+print(f"Área do retângulo: {area:.2f}")
+print(f"Perímetro do retângulo: {perimetro:.2f}")
