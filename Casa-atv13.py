7b9cc395dc5d9d6d2ea99f1e2a5b0b3bb61e8585
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:52:56 -0300

    Update Casa-atv13.py

--- Casa-atv13.py
+++ Casa-atv13.py
@@ -1,1 +1,9 @@
+s0 = 2      
+v0 = 3      
+a = 10     
 
+t = float(input("Digite o valor do tempo t (em segundos): "))
+
+s = s0 + v0 * t + 0.5 * a * (t ** 2)
+
+print(f"O valor da posição s após {t} segundos é: {s:.2f} metros")
