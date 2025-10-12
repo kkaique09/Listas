e6b2e8fef7038a591c37afc4943c813d9b10f96d
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:49:54 -0300

    Update Casa-at6.py

--- Casa-at6.py
+++ Casa-at6.py
@@ -1,1 +1,9 @@
+import math
 
+raio = float(input("Digite o valor do raio da circunferência: "))
+
+area = math.pi * (raio ** 2)
+comprimento = 2 * math.pi * raio
+
+print(f"Área do círculo: {area:.2f}")
+print(f"Comprimento da circunferência: {comprimento:.2f}")
