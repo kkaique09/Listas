38b7824d6b1df7a7c86f1d2bd1fe03190bb055f4
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:52:21 -0300

    Update Casa-atv12.py

--- Casa-atv12.py
+++ Casa-atv12.py
@@ -1,1 +1,6 @@
+espaco = float(input("Digite o espaço percorrido (em km): "))
+tempo = float(input("Digite o tempo gasto (em horas): "))
 
+velocidade_media = espaco / tempo
+
+print(f"A velocidade média é: {velocidade_media:.2f} km/h")
