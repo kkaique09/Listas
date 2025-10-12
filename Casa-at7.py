ba86d6149401cd83fd51547f7007f75af9386166
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-11 23:50:17 -0300

    Update Casa-at7.py

--- Casa-at7.py
+++ Casa-at7.py
@@ -1,1 +1,9 @@
+import math
 
+raio = float(input("Digite o valor do raio da esfera: "))
+
+volume = (4/3) * math.pi * (raio ** 3)
+area = 4 * math.pi * (raio ** 2)
+
+print(f"Volume da esfera: {volume:.2f}")
+print(f"Área da superfície da esfera: {area:.2f}")
