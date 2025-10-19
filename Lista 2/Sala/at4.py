8e8c57a121b0d0fcf3a75472930dc3de86b02ee2
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:43:33 -0300

    Update at4.py

--- Lista 2/Sala/at4.py
+++ Lista 2/Sala/at4.py
@@ -1,1 +1,13 @@
+A = float(input("Lado A: "))
+B = float(input("Lado B: "))
+C = float(input("Lado C: "))
+if A < B + C and B < A + C and C < A + B:
+    if A == B and B == C:
+        print("Triângulo equilátero")
+    elif A == B or A == C or B == C:
+        print("Triângulo isósceles")
+    else:
+        print("Triângulo escaleno")
+else:
+    print("Não é um triângulo")
 
