87de7720e2d7767f66d5ccf9a3a85ac392f9e9e2
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:42:19 -0300

    Update at2.py

--- Lista 2/Sala/at2.py
+++ Lista 2/Sala/at2.py
@@ -1,1 +1,12 @@
-
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+media = (n1 + n2) / 2
+if media >= 6:
+    print(f"Aprovado com média {media:.2f}")
+else:
+    exame = float(input("Nota do exame: "))
+    nova_media = (media + exame) / 2
+    if nova_media >= 5:
+        print(f"Aprovado em exame com média {nova_media:.2f}")
+    else:
+        print(f"Reprovado com média {nova_media:.2f}")
