c4fa49a98ffb2fdc051fe18d6f37f4fa27da59b5
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:43:47 -0300

    Implement sorting of three input values
    Sort three input values A, B, and C in ascending order.

--- Lista 2/Sala/at5.py
+++ Lista 2/Sala/at5.py
@@ -1,1 +1,10 @@
-
+A = float(input("Valor A: "))
+B = float(input("Valor B: "))
+C = float(input("Valor C: "))
+if A > B:
+    A, B = B, A
+if B > C:
+    B, C = C, B
+if A > B:
+    A, B = B, A
+print(A, B, C)
