8c5c2ffc76de94d5205d52d7f5c748a9602b6057
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:33:29 -0300

    Add rounding logic for user input grade

--- Lista 2/Casa/at3.py
+++ Lista 2/Casa/at3.py
@@ -1,1 +1,9 @@
+nota = float(input("Digite a nota: "))
+resto = nota % 1 
 
+if resto > 0.5:
+    nota_arredondada = int(nota) + 1
+else:
+    nota_arredondada = int(nota)
+
+print(f"Nota arredondada: {nota_arredondada:.1f}")
