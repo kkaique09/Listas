fd39791144fff9e273902c973e3fb8dc1bf57a74
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:35:03 -0300

    Add program to find min, mid, and max of three numbers

--- Lista 2/Casa/at4.py
+++ Lista 2/Casa/at4.py
@@ -1,1 +1,28 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+c = float(input("Digite o terceiro número: "))
 
+if a >= b and a >= c:
+    maior = a
+elif b >= a and b >= c:
+    maior = b
+else:
+    maior = c
+
+if a <= b and a <= c:
+    menor = a
+elif b <= a and b <= c:
+    menor = b
+else:
+    menor = c
+
+if a != maior and a != menor:
+    meio = a
+elif b != maior and b != menor:
+    meio = b
+else:
+    meio = c
+
+print(f"Menor número: {menor}")
+print(f"Número do meio: {meio}")
+print(f"Maior número: {maior}")
