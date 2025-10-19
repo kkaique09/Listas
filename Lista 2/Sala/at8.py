9f3a58cbfda3d5dee99454aa863fa2f18cb52973
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:44:48 -0300

    Update at8.py

--- Lista 2/Sala/at8.py
+++ Lista 2/Sala/at8.py
@@ -1,1 +1,10 @@
+a = int(input("Número 1: "))
+b = int(input("Número 2: "))
+c = int(input("Número 3: "))
+if a % 2 == 0 and a % 3 == 0:
+    print(a)
+if b % 2 == 0 and b % 3 == 0:
+    print(b)
+if c % 2 == 0 and c % 3 == 0:
+    print(c)
 
