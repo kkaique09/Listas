f27931254ce63ffc1308c0273db2515788118fa8
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:41:25 -0300

    Update at1.py

--- Lista 2/Sala/at1.py
+++ Lista 2/Sala/at1.py
@@ -1,1 +1,9 @@
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+n3 = float(input("Nota 3: "))
+media = (n1 + n2 + n3) / 3
+if media >= 6:
+    print(f"Aprovado com média {media:.2f}")
+else:
+    print(f"Reprovado com média {media:.2f}")
 
