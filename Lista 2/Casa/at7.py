f4275d4440997db731f038297b3f0d400a199531
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-18 23:37:31 -0300

    Update at7.py

--- Lista 2/Casa/at7.py
+++ Lista 2/Casa/at7.py
@@ -1,1 +1,9 @@
+codigo = int(input("Digite o código do curso (1 a 5): "))
 
+match codigo:
+    case 1: print("Engenharia")
+    case 2: print("Edificações")
+    case 3: print("Sistemas Elétricos")
+    case 4: print("Turismo")
+    case 5: print("Análise de Sistemas")
+    case _: print("Código inválido!")
