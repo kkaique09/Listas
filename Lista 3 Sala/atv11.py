a8f1061fc74fc7ed84345d0039bf21190be6b5c4
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-10-30 16:01:10 -0300

    Update atv11.py

--- Lista 3 Sala/atv11.py
+++ Lista 3 Sala/atv11.py
@@ -1,1 +1,16 @@
+numeros = []
+for i in range(5):
+    num = int(input())
+    numeros.append(num)
 
+maior = numeros[0]
+menor = numeros[0]
+
+for numero in numeros:
+    if numero > maior:
+        maior = numero
+    if numero < menor:
+        menor = numero
+
+print(maior)
+print(menor)
