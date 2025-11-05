be0b524bd65ff7535b71dbea053a431d5ae0674f
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:48:37 -0300

    Update at3.py

--- Lista 4/Casa/at3.py
+++ Lista 4/Casa/at3.py
@@ -1,1 +1,15 @@
+while True:
+    ra_inicial = input("Digite o RA inicial (9 dígitos): ")
+    if len(ra_inicial) == 9 and ra_inicial.isdigit():
+        break
+    print("Entrada inválida. Por favor, digite exatamente 9 dígitos numéricos.")
 
+parte_a_invertida = ra_inicial[0:2][::-1]
+parte_b_fixa = ra_inicial[2:7] 
+parte_c_invertida = ra_inicial[7:9][::-1]
+
+ra_novo = parte_a_invertida + parte_b_fixa + parte_c_invertida
+
+print("\n--- Resultado da Dupla Inversão do RA ---")
+print(f"RA Inicial: {ra_inicial}")
+print(f"RA Novo:    {ra_novo}")
