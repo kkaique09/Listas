c78b265c753d46854ea87f6dbdca4c055f87f973
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:41:14 -0300

    Update at3.py

--- Lista 4/Sala/at3.py
+++ Lista 4/Sala/at3.py
@@ -1,1 +1,20 @@
+# 3) Subtração de Matrizes
+A = []
+for i in range(5):
+    try:
+        A.append(float(input(f"A[{i+1}]: ")))
+    except ValueError:
+        A.append(0.0)
 
+B = []
+for i in range(5):
+    try:
+        B.append(float(input(f"B[{i+1}]: ")))
+    except ValueError:
+        B.append(0.0)
+
+C = []
+for i in range(5):
+    C.append(A[i] - B[i])
+
+print("Matriz C (A - B):", C)
