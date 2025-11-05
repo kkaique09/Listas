67544092cf3b194757ac986ebc5120c11f233ae0
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:39:46 -0300

    Update at1.py

--- Lista 4/Sala/at1.py
+++ Lista 4/Sala/at1.py
@@ -1,1 +1,13 @@
+# 1) Multiplicação por 3
+A = []
+for i in range(5):
+    try:
+        A.append(float(input(f"A[{i+1}]: ")))
+    except ValueError:
+        A.append(0.0)
 
+B = []
+for elemento_a in A:
+    B.append(elemento_a * 3)
+
+print("Matriz B (A * 3):", B)
