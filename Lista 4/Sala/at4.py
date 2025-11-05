3b3a970a5958f2c9787d26f542fa5e4e0f851fdc
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:41:31 -0300

    Update at4.py

--- Lista 4/Sala/at4.py
+++ Lista 4/Sala/at4.py
@@ -1,1 +1,18 @@
+# 4) Junção de Matrizes (A de 5 e B de 5)
+A = []
+for i in range(5):
+    try:
+        A.append(float(input(f"A[{i+1}]: ")))
+    except ValueError:
+        A.append(0.0)
 
+B = []
+for i in range(5):
+    try:
+        B.append(float(input(f"B[{i+1}]: ")))
+    except ValueError:
+        B.append(0.0)
+
+C = A + B
+
+print("Matriz C (Junção A + B):", C)
