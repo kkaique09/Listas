8d12510b76511f1118f140d9641a9f36b60a4955
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:42:22 -0300

    Update at7.py

--- Lista 4/Sala/at7.py
+++ Lista 4/Sala/at7.py
@@ -1,1 +1,11 @@
+# 7) Inversão de Matriz
+A = []
+for i in range(10):
+    try:
+        A.append(float(input(f"A[{i+1}]: ")))
+    except ValueError:
+        A.append(0.0)
 
+B = A[::-1]
+
+print("Matriz B (Invertida):", B)
