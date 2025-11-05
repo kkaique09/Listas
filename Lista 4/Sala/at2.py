58c9a1abd1ef38b930bc7f590ba7e1d02252928c
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:40:12 -0300

    Update at2.py

--- Lista 4/Sala/at2.py
+++ Lista 4/Sala/at2.py
@@ -1,1 +1,20 @@
+# 2) Fatorial dos Elementos
+import math
 
+def calcular_fatorial(n):
+    if n < 0:
+        return "Inválido"
+    return math.factorial(int(n))
+
+A = []
+for i in range(6):
+    try:
+        A.append(int(input(f"A[{i+1}]: ")))
+    except ValueError:
+        A.append(0)
+
+B = []
+for elemento_a in A:
+    B.append(calcular_fatorial(elemento_a))
+
+print("Matriz B (Fatorial de A):", B)
