1ae21c4c4e151b92310d3fdba27d1b658f08811c
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:42:02 -0300

    Update at5.py

--- Lista 4/Sala/at5.py
+++ Lista 4/Sala/at5.py
@@ -1,1 +1,8 @@
+# 5) Junção de Matrizes (A de 20 e B de 30)
+# Simulação de leitura para 50 elementos
+A = list(range(20))
+B = list(range(100, 130))
 
+C = A + B
+
+print(f"Matriz C (50 elementos): {C[:5]} ... {C[-5:]}")
