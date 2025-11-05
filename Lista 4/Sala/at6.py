f0558829dda3642ff71224b1c8917761c55d073d
kkaique09 <kaique.avante@aluno.ifsp.edu.br>
2025-11-04 22:42:12 -0300

    Update at6.py

--- Lista 4/Sala/at6.py
+++ Lista 4/Sala/at6.py
@@ -1,1 +1,13 @@
+# 6) Quadrado dos Elementos
+A = []
+for i in range(8):
+    try:
+        A.append(float(input(f"A[{i+1}]: ")))
+    except ValueError:
+        A.append(0.0)
 
+B = []
+for elemento_a in A:
+    B.append(elemento_a ** 2)
+
+print("Matriz B (A²):", B)
